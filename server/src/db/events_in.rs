7bfafbc0e9e738fd53bf38ca3e48f024bfd7bcf1
Rithvik1124 <pvrithvik456@gmail.com>
2026-07-27 21:10:24 +0530

    Restructure: ToDo

--- server/src/db/events_in.rs
+++ server/src/db/events_in.rs
@@ -4,38 +4,16 @@ use serde::{Deserialize, Serialize};
 use crate::telemetry::TelemetryEvent;
 use std::hash::{DefaultHasher, Hash, Hasher};
 const EVENTS_TABLE: TableDefinition<u64, &[u8]> = TableDefinition::new("events_in");
+use std::sync::LazyLock;
+
+// Was calling setdb in the function each time so turned it into static
+const PATH: &str = "events_in.redb";
 
 
-// #[derive(Serialize, Deserialize, Hash, Debug)]
-// pub struct TelemetryEvent {
-//     pub event_type: String,
-//     pub pid: u32,
-//     pub ppid: u32,
-//     pub uid: u32,
-//     pub gid: u32,
-//     pub tgid: u64,
-
-//     pub comm: String,
-//     pub filename: String,
-
-//     pub dst_ip: String,
-//     pub dst_port: String,
-
-//     pub time_stamp: String,
-// }
-// impl Hash for GenEvent {
-//     fn hash<H: Hasher>(&self, state: &mut H) {
-//         self.pid.hash(state);
-//         self.time_stamp.hash(state);
-//         self.filename.hash(state);
-//     }
-// }
-
-// fn calculate_hash<T: Hash>(t: &T) -> u64 {
-//     let mut s = DefaultHasher::new();
-//     t.hash(&mut s);
-//     s.finish()
-// }
+static DB: LazyLock<Database> = LazyLock::new(|| {
+    Database::create(PATH)
+        .expect("Failed to create DB")
+});
 
 fn calculate_hash<T: Hash>(value: &T) -> u64 {
     let mut hasher = DefaultHasher::new();
@@ -44,13 +22,10 @@ fn calculate_hash<T: Hash>(value: &T) -> u64 {
 }
 
 
-
-
 pub fn write_event(event: TelemetryEvent) -> Result<(), Box<dyn std::error::Error>>{
-    let db = crate::db::initialize_db::set_db()?;
     println!("Starting write");
     let event_id = calculate_hash(&event);
-    let write_txn = db.begin_write().unwrap();
+    let write_txn = DB.begin_write().unwrap();
     {
         let mut table = write_txn.open_table(EVENTS_TABLE)?;
         
@@ -66,7 +41,7 @@ pub fn write_event(event: TelemetryEvent) -> Result<(), Box<dyn std::error::Erro
     write_txn.commit()?;
     // Ok(())
 
-    let read_txn = db.begin_read()?;
+    let read_txn = DB.begin_read()?;
     println!("Opened DB");
     let table = read_txn.open_table(EVENTS_TABLE)?;
     // // Clone the bytes to own them outside the transaction scope.
--- server/src/telemetry.rs
+++ server/src/telemetry.rs
@@ -3,6 +3,45 @@ use std::hash::{DefaultHasher, Hash, Hasher};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Hash, Debug)]
+// struct SigmaResults{
+//     rule_id: String,
+//     rule_name: String,
+//     rule_triggers: String,
+// }
+// #[derive(Serialize, Deserialize, Hash, Debug)]
+
+// struct YaraResults{
+//     rule_id: String,
+//     rule_name: String,
+//     rule_triggers: String,
+// }
+// #[derive(Serialize, Deserialize, Hash, Debug)]
+
+// struct IOCResults{
+//     rule_id: String,
+//     rule_name: String,
+//     rule_triggers: String,
+// }
+//#[derive(Serialize, Deserialize)]
+
+struct DetectionResult{
+    rule_id: String,
+    rule_name: String,
+    rule_triggers: String,
+}
+
+#[derive(Serialize, Deserialize,Debug, Hash)]
+struct AnalysisResult{
+    is_mal: bool,
+    detection_rule:String,
+    sigma_results: Vec<DetectionResult>,
+    yara_results: Vec<DetectionResult>,
+    ioc_results: Vec<DetectionResult>,
+}
+
+
+
+#[derive(Serialize, Deserialize, Hash, Debug)]
 pub struct TelemetryEvent {     
     pub event_type: String,         
     pub pid: u32,
@@ -10,12 +49,19 @@ pub struct TelemetryEvent {
     pub uid: u32,
     pub gid: u32,
     pub tgid: u64,
-
     pub comm: String,
     pub filename: String,
-
     pub dst_ip: String, //max 15 bytes
     pub dst_port: String, //max 5 bytes
-
-    pub time_stamp: String,
+    pub time_stamp:String,
+    pub analysis_result: AnalysisResult,
 }
+
+// #[derive(Serialize, Deserialize, Hash, Debug)]
+// pub struct FullEvent{
+//     pub telemetry: TelemetryEvent,
+//     pub sigma_results: Option<AnalysisResult>,
+//     pub yara_results: Option<AnalysisResult>,
+//     pub ioc_results: Option<AnalysisResult>,
+//     pub time_stamp: String,
+// }
