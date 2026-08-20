c88ee5a6bad77961a5eb7553f17fd34ca642252b
Rithvik1124 <pvrithvik456@gmail.com>
2026-08-20 14:17:00 +0530

    Basic IOC #1: File Hash, Ipsum Blocklist

--- src/node_roles/ioc/ioc_detection.rs
+++ src/node_roles/ioc/ioc_detection.rs
@@ -1,1 +1,52 @@
-use crate::ioc::IPSUM_IOC_MAP;
+use crate::ioc::{BLOCKLIST_IP_IOC_MAP, FILE_HASHES_MAP};
+use sha256::{digest, try_digest};
+use std::{
+    collections::HashMap,
+    io::{BufRead, BufReader},
+    fs::File,
+    net::Ipv4Addr,
+    sync::LazyLock,
+    path::Path,
+};
+
+use reqwest::blocking::get;
+
+enum FileHashStatus{
+    Benign,
+    Malicious
+}
+
+#[derive(PartialEq, Debug)]
+
+pub enum FileHashStatus {
+    HashHit,
+    NoHashMatched,
+}
+
+pub enum BlockedIPStatus{
+    IPHit,
+    NoIPMatched,
+}
+
+
+fn get_file_hash(dir: &str) -> String {
+    let input = Path::new(dir);
+    try_digest(input).unwrap()
+}
+
+fn blocked_ip_check(ip: IpAddr)-> BlockedIPStatus{
+    if BLOCKLIST_IP_IOC_MAP.contains_key(&file_hash) {
+        BlockedIPStatus::IPHit
+    } else {
+        BlockedIPStatus::NoIPMatched
+    }
+}
+
+fn check_file_hash(file_hash: String) -> FileHashStatus {
+    if FILE_HASHES_MAP.contains_key(&file_hash) {
+        FileHashStatus::HashHit
+    } else {
+        FileHashStatus::NoHashMatched
+    }
+}
+
--- src/node_roles/ioc/mod.rs
+++ src/node_roles/ioc/mod.rs
@@ -1,21 +1,41 @@
 use std::{
-    collections::HashMap,
-    net::Ipv4Addr,
-    sync::LazyLock,
+    collections::HashMap, fs::{File}, hash::Hash, net::Ipv4Addr, sync::LazyLock, io::{BufRead, BufReader}
 };
 
 use reqwest::blocking::get;
 
 /// Global IOC map:
 /// IPSum -> threat level (1–8)
-pub static IPSUM_IOC_MAP: LazyLock<HashMap<Ipv4Addr, u8>> = LazyLock::new(|| {
+pub static BLOCKLIST_IP_IOC_MAP: LazyLock<HashMap<Ipv4Addr, u8>> = LazyLock::new(|| {
     load_ipsum_ioc()
 });
+
+pub static FILE_HASHES_MAP: LazyLock<HashMap<String, u8>> = LazyLock::new(|| {
+    load_file_hashes("file_hashes/full_sha256.txt")
+});
+
+fn load_file_hashes(dir: &str) -> HashMap<String, u8> {
+    let mut file_hashes = HashMap::new();
+
+    let file = File::open(dir).unwrap();
+    let reader = BufReader::new(file);
+
+    println!("Reading file line-by-line:\n");
+
+    for line_result in reader.lines() {
+        let line = line_result.unwrap();
+        let line = line.trim().to_string(); // FIX: must own String
+
+        file_hashes.insert(line, 1);
+    }
+
+    file_hashes
+}
+
 
 fn load_ipsum_ioc() -> HashMap<Ipv4Addr, u8> {
     let mut map: HashMap<Ipv4Addr, u8> = HashMap::new();
 
-    // IPsum levels 1 to 8
     for level in 1..=8 {
         let url = format!(
             "https://raw.githubusercontent.com/stamparm/ipsum/master/levels/{}.txt",
